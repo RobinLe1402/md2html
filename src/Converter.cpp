c703f733ec8f19a74792eb59df911a269c7c88a7
RobinLe <23437505+RobinLe1402@users.noreply.github.com>
2025-03-24 00:24:25 +0100

    fixed code block by indentation

--- src/Converter.cpp
+++ src/Converter.cpp
@@ -55,24 +55,40 @@ void Converter::processBlock(const std::vector<std::string> &oTextBlock)
 
             ++iLine;
 
+            size_t iFirstEmpty = 1;
             while (iLine < oTextBlock.size())
             {
                 const auto &sLine = oTextBlock[iLine];
 
                 if (std::regex_search(sLine, oMatch, RegEx::CODEBLOCK_INDENTED))
+                {
                     oCodeBlock.push_back(oMatch[1].str());
+                    iFirstEmpty = oCodeBlock.size();
+                }
+                else if (EmptyLine(sLine))
+                    oCodeBlock.push_back("");
                 else
                     break;
 
                 ++iLine;
             }
 
+            oCodeBlock.erase(oCodeBlock.begin() + iFirstEmpty, oCodeBlock.end());
+
             m_oOutput << "<pre>";
             for (size_t i = 0; i + 1 < oCodeBlock.size(); ++i)
             {
                 m_oOutput << oCodeBlock[i] << '\n';
             }
-            m_oOutput << oCodeBlock[oCodeBlock.size() - 1] << "</pre>";
+            m_oOutput << oCodeBlock[oCodeBlock.size() - 1] << "</pre>\n";
+        }
+
+
+
+        else
+        {
+            m_oOutput << sLine << '\n';
+            ++iLine;
         }
     }
 }
