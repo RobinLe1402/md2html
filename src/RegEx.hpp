0fee27f6fca8b7a7733f0ec291626f0dc6cc80d6
RobinLe1402 <accounts.github@robinle.net>
2025-03-27 08:30:32 +0100

    update indentation

--- src/RegEx.hpp
+++ src/RegEx.hpp
@@ -16,9 +16,9 @@ namespace RegEx
 
 
 
-    const auto CODEBLOCK_INDENTED  = std::regex(R"RegEx(^\s{4}(.+)$)RegEx", std::regex::optimize);
+    const auto CODEBLOCK_INDENTED = std::regex(R"RegEx(^\s{4}(.+)$)RegEx", std::regex::optimize);
 
-    const auto BLOCKQUOTE          = std::regex(R"RegEx(^ *> ?(.*)$)RegEx", std::regex::optimize);
+    const auto BLOCKQUOTE = std::regex(R"RegEx(^ *> ?(.*)$)RegEx", std::regex::optimize);
 
     const auto CODEBLOCK_BACKTICKS_OPEN =
         std::regex(R"RegEx(^ *```([a-z]*)$)RegEx", std::regex::optimize | std::regex::icase);
@@ -28,8 +28,8 @@ namespace RegEx
     const auto TABLE_LINE_HEAD = std::regex(R"RegEx(^\s*\|(.*\|)\s*$)RegEx",  std::regex::optimize);
     const auto TABLE_LINE_HEAD_SEPARATOR =
         std::regex(R"RegEx(^\s*\|(\s*:?-+:?\s*\|)+$)RegEx", std::regex::optimize);
-    const auto TABLE_LINE      = std::regex(R"RegEx(^\s*\|(.*\|)?\s*$)RegEx", std::regex::optimize);
-    const auto TABLE_ITEM      = std::regex(R"RegEx(\s*([^|]*?)\s*\|)RegEx",  std::regex::optimize);
+    const auto TABLE_LINE = std::regex(R"RegEx(^\s*\|(.*\|)?\s*$)RegEx", std::regex::optimize);
+    const auto TABLE_ITEM = std::regex(R"RegEx(\s*([^|]*?)\s*\|)RegEx",  std::regex::optimize);
     const auto TABLE_HEAD_SEPARATOR = std::regex(R"RegEx((:?)-+(:?))RegEx", std::regex::optimize);
 
 }
